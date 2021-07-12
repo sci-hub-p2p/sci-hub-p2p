333207a61b851389b9f97a4b6645e58b59764748
Trim21 <trim21.me@gmail.com>
2021-07-13 02:48:05 +0800

    replace with a small torrent

--- fetch.bash
+++ fetch.bash
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-curl https://libgen.rs/scimag/repository_torrent/sm_83500000-83599999.torrent \
-  -o ./tests/fixtures/sm_83500000-83599999.torrent
+curl https://libgen.rs/scimag/repository_torrent/sm_00900000-00999999.torrent \
+  -o ./tests/fixtures/sm_00900000-00999999.torrent
