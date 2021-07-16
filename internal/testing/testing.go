e7edd111bfeca06de2b14d88ee43c5e6aa1fd3c5
Trim21 <i@trim21.me>
2021-07-16 23:35:04 +0800

    fix test

--- internal/testing/testing.go
+++ internal/testing/testing.go
@@ -23,7 +23,7 @@ import (
 
 func init() {
 	_, filename, _, _ := runtime.Caller(0)
-	dir := path.Join(path.Dir(filename), "..")
+	dir := path.Join(path.Dir(filename), "..", "..")
 	if err := os.Chdir(dir); err != nil {
 		panic(err)
 	}
