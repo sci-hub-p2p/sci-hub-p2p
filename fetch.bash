680394ce49864b182a7d33de6d437d498ed3fc27
Trim21 <trim21.me@gmail.com>
2021-07-18 23:38:03 +0800

    update

--- fetch.bash
+++ fetch.bash
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-#curl https://libgen.rs/scimag/repository_torrent/sm_00900000-00999999.torrent \
-#  -o ./testdata/sm_00900000-00999999.torrent
+curl https://libgen.rs/scimag/repository_torrent/sm_00900000-00999999.torrent \
+  -o ./testdata/sm_00900000-00999999.torrent
