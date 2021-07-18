062069c6f4542a6c95c37aa37a2a4cad111e0444
Trim21 <trim21.me@gmail.com>
2021-07-18 16:51:35 +0800

    un expected change

--- fetch.bash
+++ fetch.bash
@@ -1,13 +1,4 @@
 #!/usr/bin/env bash
 
-#curl https://libgen.rs/scimag/repository_torrent/sm_00900000-00999999.torrent \
-#  -o ./testdata/sm_00900000-00999999.torrent
-#
-
-#!/usr/bin/env bash
-
-FILES="$HOME/repository_torrent/sm_*.torrent"
-for f in $FILES; do
-  echo "Processing $f file..."
-  ./dist/sci-hub_windows_64.exe indexes gen -t "$f" -d '\\OMV\3t\sci-hub' -n 1
-done
+curl https://libgen.rs/scimag/repository_torrent/sm_00900000-00999999.torrent \
+  -o ./testdata/sm_00900000-00999999.torrent
