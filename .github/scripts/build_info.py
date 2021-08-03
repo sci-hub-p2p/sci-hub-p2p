1c48a800d62afb27cef8028620e7c58d487de574
Trim21 <trim21.me@gmail.com>
2021-08-03 17:42:48 +0800

    fix

--- .github/scripts/build_info.py
+++ .github/scripts/build_info.py
@@ -17,9 +17,9 @@ elif match := re.match("refs/pull/(.*)/merge", ref):
     ref = "pr-" + match.group(1)
 
 content = f"""
-REF="{ref}"
-SHA="{SHA}"
-TIME="{build_time}"
+REF={ref}
+SHA={SHA}
+TIME={build_time}
 """
 
 with open(os.getenv("GITHUB_ENV"), "a+", encoding="utf8") as f:
