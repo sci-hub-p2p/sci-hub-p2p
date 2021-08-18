94b2221c29311cf631d4f2d752f750b1a0754169
Trim21 <trim21.me@gmail.com>
2021-08-19 01:06:09 +0800

    fix

---
+++ tools/tools.go
@@ -0,0 +1,20 @@
+// Copyright 2021 Trim21 <trim21.me@gmail.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU General Public License for more details.
+
+//nolint
+package tools
+
+// A dummy package to make `go mod tidy` and go install happy.
+
+import (
+	_ "github.com/GeertJohan/go.rice/rice"
+)
